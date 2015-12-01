a3abf8238c738863c99b9710e3fa16e36b38070a
drwebmaker <valeriy.abornev@globallogic.com>
2015-12-01 17:50:01 +0200

    added  wabpack config

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    entry: {
+        app: __dirname + '/src/example/main.js'
+    },
+    output: {
+        filename: __dirname + '/src/example/app.js'
+    },
+    devtool: 'source-map'
+};
